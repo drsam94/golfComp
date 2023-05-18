2e0906b743b2f360dfab94eff56ad776d99de645
Sam Donow <drsam94@gmail.com>
2023-05-18 04:05:34 -0400

    Further refinemnet of APL and porting of test2.apl approach to C

--- example_solutions/Cpp/test2.cc
+++ example_solutions/Cpp/test2.cc
@@ -1,1 +1,1 @@
-long ans(int s,int e){long r=0,w=0,d=0,m=0,y=1900;while(y<=e){r+=(w=++w%7)==5&d==12&y>=s;y+=!(d*=++d<30+(2773>>m&1)-(m==1)*(2-(y%4<1&(y%100||y%400<1))))&&!(m*=12>++m);}return r;}
+long ans(int s,int e){long r=0,w=0,d=0,m=0,y=1900;while(y<=e){r+=(w=++w%7)==5&d==12&y>=s;y+=!(d*=++d<30+(2773>>m&1)-(m==1)*(2-(y%4<1&(y%25||y%16<1))))&&!(m*=12>++m);}return r;}
